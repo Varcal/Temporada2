416bdf5536436da91dba4552c954afa3f5387b57
Cleber Varcal <varcal.cleber@gmail.com>
2014-06-02 01:55:36 -0300

    2 Commit
    Implementação PedidoRepositorio e RepositorioNegocios

---
+++ AcessoBancoDados/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessoBancoDados.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        string Salvar(T entidade);
+        string Excluir(T entidade);
+        IList<T> BuscarId(int id);
+        IList<T> BuscarNome(string arg);
+        IList<T> BuscarTodos();
+    }
+}
